d0ce29ec95f407660585c23e9cea4313ea2489af
Kaushal Shukla <103252985+Delltronex@users.noreply.github.com>
2026-01-21 23:24:20 +0530

    Update llm.py

--- llm.py
+++ llm.py
@@ -1,15 +1,12 @@
+
 import os
-from dotenv import load_dotenv
 import google.generativeai as genai
 
-# Load environment variables
-load_dotenv()
-
-# Get API key from .env
+# Get API key from environment (Render / local)
 API_KEY = os.getenv("GEMINI_API_KEY")
 
 if not API_KEY:
-    raise ValueError("GEMINI_API_KEY not found in .env file")
+    raise ValueError("GEMINI_API_KEY not found in environment variables")
 
 # Configure Gemini
 genai.configure(api_key=API_KEY)
